3c30e1ea4bedc7b0daf142d02ccda380c8b73496
khlee <panpan68@naver.com>
2023-06-29 18:33:12 +0900

    스태틱 파일 관리

--- pybo/urls.py
+++ pybo/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'pybo'
 
 urlpatterns = [
+    # 매핑주소, controller의 메서드, 별칭
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
+    path('answer/create/<int:question_id>', views.answer_create, name='answer_create')
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 # from django.http import HttpResponse #삭제
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Question
 
 
@@ -11,6 +12,15 @@ def index(request):
 
 
 def detail(request, question_id):
+    # DB에 없는 ID를 url에 입력하여 500 Error발생시 그 에러를 404로 바꿔주는 것
+    #get_object_or_404(객체, pk값) -> 객체 return
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    # set질문자의 답변(textarea의 name으로 전달받은 value값)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question_id)
